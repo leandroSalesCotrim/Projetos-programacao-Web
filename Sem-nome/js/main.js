606264e73365321b2f1695b853ed7d18536c9275
Leandro de Sales Cotrim <56699403+leandroSalesCotrim@users.noreply.github.com>
2021-01-31 01:35:21 -0300

    quase finalizei o menu,mudei a cor do site e acresentei algumas animações
    falta deixar o site mais responsivel para finalizar o menu e a parte inicial da pagina

--- Sem-nome/js/main.js
+++ Sem-nome/js/main.js
@@ -2,7 +2,7 @@ $(window).on("scroll touchmove", function() {
     
     if ($(document).scrollTop() >= $("#inicioSec").position().top) {
         $('.linkInicio').css('text-decoration-line', 'underline');
-        $('.linkInicio').css('text-decoration-color', '#00BD32');
+        $('.linkInicio').css('text-decoration-color', '#F2CB05');
         $('.linkInicio').css('text-decoration-thickness', '13%');
         $('.linkProjeto').css('text-decoration', 'none');
         $('.linkSobre').css('text-decoration', 'none');
@@ -11,7 +11,7 @@ $(window).on("scroll touchmove", function() {
 
     if ($(document).scrollTop() >= $("#projetosSec").position().top) {
       $('.linkProjeto').css('text-decoration-line', 'underline');
-      $('.linkProjeto').css('text-decoration-color', '#00BD32');
+      $('.linkProjeto').css('text-decoration-color', '#F29F05');
       $('.linkProjeto').css('text-decoration-thickness', '13%');
       $('.linkInicio').css('text-decoration', 'none');
       $('.linkSobre').css('text-decoration', 'none');
@@ -20,7 +20,7 @@ $(window).on("scroll touchmove", function() {
  
     if ($(document).scrollTop() >= $("#sobreSec").position().top) {
         $('.linkSobre').css('text-decoration-line', 'underline');
-        $('.linkSobre').css('text-decoration-color', '#00BD32');
+        $('.linkSobre').css('text-decoration-color', '#F2CB05');
         $('.linkSobre').css('text-decoration-thickness', '13%');
         $('.linkInicio').css('text-decoration', 'none');
         $('.linkProjeto').css('text-decoration', 'none');
@@ -29,7 +29,7 @@ $(window).on("scroll touchmove", function() {
 
       if ($(document).scrollTop() >= $("#contatoSec").position().top) {
         $('.linkContato').css('text-decoration-line', 'underline');
-        $('.linkContato').css('text-decoration-color', '#00BD32');
+        $('.linkContato').css('text-decoration-color', '#F2CB05');
         $('.linkContato').css('text-decoration-thickness', '13%');
         $('.linkInicio').css('text-decoration', 'none');
         $('.linkSobre').css('text-decoration', 'none');
@@ -42,32 +42,32 @@ $(window).on("scroll touchmove", function() {
   }
  
   function scroll(){
-    let btnTop = document.getElementById("btnTop");
-    let divMenu = document.getElementById("divMenu");
-    let linkInicio = document.getElementById("linkInicio");
-    let linkProjeto = document.getElementById("linkProjeto");
-    let linkSobre = document.getElementById("linkSobre");
-    let linkContato = document.getElementById("linkContato");
+    btnTop = document.getElementById("btnTop");
+    divMenu = document.getElementById("divMenu");
+    fundoLogo = document.getElementById("divFundo");
+    linkInicio = document.getElementById("linkInicio");
+    linkProjeto = document.getElementById("linkProjeto");
+    linkSobre = document.getElementById("linkSobre");
+    linkContato = document.getElementById("linkContato");
+
     if(document.documentElement.scrollTop > 50){
       btnTop.style.display = "block";
       btnTop.style.animationDuration = "0.7s";
       divMenu.style.transition = "0.3s";
-      divMenu.style.backgroundColor = "#1802db1e";
+      divMenu.style.backgroundColor = "#4040402d";
       divMenu.style.height = "4em";
-      linkInicio.style.marginTop= "1em"
-      linkProjeto.style.marginTop= "0.60em";
-      linkSobre.style.marginTop= "0.60em";
-      linkContato.style.marginTop= "0.60em";
+      fundoLogo.style.width = "10%";
+      linkInicio.style.marginTop = "1em"
+      linkProjeto.style.marginTop = "0.60em";
+      linkSobre.style.marginTop = "0.60em";
+      linkContato.style.marginTop = "0.60em";
 
     }else{
       btnTop.style.display = "none";
       divMenu.style.transition = "0.7s";
-      divMenu.style.backgroundColor = "#1802db";
+      divMenu.style.backgroundColor = "#0D0D0D";
       divMenu.style.height = "6em";
-      linkInicio.style.marginTop= "2em"
-      linkProjeto.style.marginTop= "1.6em";
-      linkSobre.style.marginTop= "1.6em";
-      linkContato.style.marginTop= "1.6em";
+      fundoLogo.style.width = "20%";
       //0.60 e 1em
     }
   }
