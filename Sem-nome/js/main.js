923e4643dad0a5be846980546c816f284b7561e2
Leandro de Sales Cotrim <56699403+leandroSalesCotrim@users.noreply.github.com>
2021-02-07 22:34:17 -0300

    arrumei a parte "sobre nós" e algumas outras partes visuais
    mudei a cor de algumas coisas e removi a animação do fundo do botão de subir a pagina, também alterei a cor do botão e do fundo do mesmo, acrescentei a seção "sobre nós" e no momento não pretendo mexer mais nessa seção

--- Sem-nome/js/main.js
+++ Sem-nome/js/main.js
@@ -7,8 +7,16 @@ $(window).on("scroll touchmove", function() {
         $('.linkProjeto').css('text-decoration', 'none');
         $('.linkSobre').css('text-decoration', 'none');
         $('.linkContato').css('text-decoration', 'none');
-      } 
-
+      }else 
+    if ($(document).scrollTop() >= $("#sobreSec").position().top) {
+        $('.linkSobre').css('text-decoration-line', 'underline');
+        $('.linkSobre').css('text-decoration-color', '#F2CB05');
+        $('.linkSobre').css('text-decoration-thickness', '13%');
+        $('.linkInicio').css('text-decoration', 'none');
+        $('.linkProjeto').css('text-decoration', 'none');
+        $('.linkContato').css('text-decoration', 'none');
+    } 
+      
     if ($(document).scrollTop() >= $("#projetosSec").position().top) {
       $('.linkProjeto').css('text-decoration-line', 'underline');
       $('.linkProjeto').css('text-decoration-color', '#F2CB05');
@@ -17,16 +25,6 @@ $(window).on("scroll touchmove", function() {
       $('.linkSobre').css('text-decoration', 'none');
       $('.linkContato').css('text-decoration', 'none');
     }
- 
-    if ($(document).scrollTop() >= $("#sobreSec").position().top) {
-        $('.linkSobre').css('text-decoration-line', 'underline');
-        $('.linkSobre').css('text-decoration-color', '#F2CB05');
-        $('.linkSobre').css('text-decoration-thickness', '13%');
-        $('.linkInicio').css('text-decoration', 'none');
-        $('.linkProjeto').css('text-decoration', 'none');
-        $('.linkContato').css('text-decoration', 'none');
-      } 
-
       if ($(document).scrollTop() >= $("#contatoSec").position().top) {
         $('.linkContato').css('text-decoration-line', 'underline');
         $('.linkContato').css('text-decoration-color', '#F2CB05');
@@ -43,21 +41,21 @@ $(window).on("scroll touchmove", function() {
  
   function scroll(){
     btnTop = document.getElementById("btnTop");
+    divFundoBtn = document.getElementById("divFundoBtn");
     divMenu = document.getElementById("divMenu");
-    linkInicio = document.getElementById("linkInicio");
-    linkProjeto = document.getElementById("linkProjeto");
-    linkSobre = document.getElementById("linkSobre");
-    linkContato = document.getElementById("linkContato");
 
-    if(document.documentElement.scrollTop > 50){
+
+    if(document.documentElement.scrollTop > 95){
       btnTop.style.display = "block";
       btnTop.style.animationDuration = "0.7s";
+      divFundoBtn.style.display = "block";
       divMenu.style.transition = "0.3s";
-      divMenu.style.backgroundColor = "#4040402d";
+      divMenu.style.backgroundColor = "#0d0d0da6";
       divMenu.style.height = "10%";
-
     }else{
       btnTop.style.display = "none";
+      btnTop.style.transition = "0.7s";
+      divFundoBtn.style.display = "none";
       divMenu.style.transition = "0.7s";
       divMenu.style.backgroundColor = "#0d0d0d00";
       divMenu.style.height = "14.5%";
