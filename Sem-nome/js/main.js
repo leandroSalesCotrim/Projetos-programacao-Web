28f61022291a3dfef06fd4779771c2aef511bfcd
Leandro de Sales Cotrim <56699403+leandroSalesCotrim@users.noreply.github.com>
2021-02-12 23:44:10 -0300

    mais mudanças...
    acho que terminei o layout do formulario de contato,e também terminei as animações dos inputs, só preciso deixar responsivo agora
    Também removi algumas partes inuteis do codigo e diminui codigos que estavam extensos

--- Sem-nome/js/main.js
+++ Sem-nome/js/main.js
@@ -7,7 +7,7 @@ $(window).on("scroll touchmove", function() {
         $('.linkProjeto').css('text-decoration', 'none');
         $('.linkSobre').css('text-decoration', 'none');
         $('.linkContato').css('text-decoration', 'none');
-      }else 
+      }
     if ($(document).scrollTop() >= $("#sobreSec").position().top) {
         $('.linkSobre').css('text-decoration-line', 'underline');
         $('.linkSobre').css('text-decoration-color', '#F2CB05');
@@ -25,14 +25,14 @@ $(window).on("scroll touchmove", function() {
       $('.linkSobre').css('text-decoration', 'none');
       $('.linkContato').css('text-decoration', 'none');
     }
-      if ($(document).scrollTop() >= $("#contatoSec").position().top) {
-        $('.linkContato').css('text-decoration-line', 'underline');
-        $('.linkContato').css('text-decoration-color', '#F2CB05');
-        $('.linkContato').css('text-decoration-thickness', '13%');
-        $('.linkInicio').css('text-decoration', 'none');
-        $('.linkSobre').css('text-decoration', 'none');
-        $('.linkProjeto').css('text-decoration', 'none');
-      }  
+    if ($(document).scrollTop() >= $("#contatoSec").position().top) {
+      $('.linkContato').css('text-decoration-line', 'underline');
+      $('.linkContato').css('text-decoration-color', '#F2CB05');
+      $('.linkContato').css('text-decoration-thickness', '13%');
+      $('.linkInicio').css('text-decoration', 'none');
+      $('.linkSobre').css('text-decoration', 'none');
+      $('.linkProjeto').css('text-decoration', 'none');
+    }  
   });
 
   window.onscroll = function(){
@@ -63,4 +63,78 @@ $(window).on("scroll touchmove", function() {
   }
   function subir(){
     document.documentElement.scrollTop = 0 ;
+  }
+  function foco1(){
+    lblNome = document.getElementById("lblNome");
+    txtNome = document.getElementById("txtNome");
+    lblNome.style.marginTop = "3vmax";
+    lblNome.style.color = "#F2CB05";
+    lblNome.style.transition = "0.3s";
+    txtNome.style.borderColor = "#F2CB05";
+    txtNome.style.transition = "0.2s";
+  }
+  function desfoque(){
+    if(document.getElementById("txtNome").value == ""){
+      lblNome = document.getElementById("lblNome");
+      txtNome = document.getElementById("txtNome");
+      lblNome.style.marginTop = "4.8vmax";
+      lblNome.style.color = "#404040";
+      lblNome.style.transition = "0.3s";
+      txtNome.style.borderColor = "#626262";
+      txtNome.style.transition = "0.2s";
+    }
+    if(document.getElementById("txtEmail").value == ""){
+      lblEmail = document.getElementById("lblEmail");
+      txtEmail = document.getElementById("txtEmail");
+      lblEmail.style.marginTop = "1.9vmax";
+      lblEmail.style.color = "#404040";
+      lblEmail.style.transition = "0.3s";
+      txtEmail.style.borderColor = "#626262";
+      txtEmail.style.transition = "0.2s";
+    }
+    if(document.getElementById("txtAssunto").value == ""){
+      lblAssunto = document.getElementById("lblAssunto");
+      txtAssunto = document.getElementById("txtAssunto");
+      lblAssunto.style.marginTop = "1.9vmax";
+      lblAssunto.style.color = "#404040";
+      lblAssunto.style.transition = "0.3s";
+      txtAssunto.style.borderColor = "#626262";
+      txtAssunto.style.transition = "0.2s";
+    }
+    if(document.getElementById("txtMensagem").value == ""){
+      lblMensagem = document.getElementById("lblMensagem");
+      txtMensagem = document.getElementById("txtMensagem");
+      lblMensagem.style.color = "#404040";
+      lblMensagem.style.transition = "0.3s";
+      txtMensagem.style.borderColor = "#626262";
+      txtMensagem.style.transition = "0.2s";
+    }
+    
+
+  }
+  function foco2(){
+    lblEmail = document.getElementById("lblEmail");
+    txtEmail = document.getElementById("txtEmail");
+    lblEmail.style.marginTop = "0.5vmax";
+    lblEmail.style.color = "#F2CB05";
+    lblEmail.style.transition = "0.3s";
+    txtEmail.style.borderColor = "#F2CB05";
+    txtEmail.style.transition = "0.2s";
+  }
+  function foco3(){
+    lblAssunto = document.getElementById("lblAssunto");
+    txtAssunto = document.getElementById("txtAssunto");
+    lblAssunto.style.marginTop = "0.5vmax";
+    lblAssunto.style.color = "#F2CB05";
+    lblAssunto.style.transition = "0.3s";
+    txtAssunto.style.borderColor = "#F2CB05";
+    txtAssunto.style.transition = "0.2s";
+  }
+  function foco4(){
+    lblMensagem = document.getElementById("lblMensagem");
+    txtMensagem = document.getElementById("txtMensagem");
+    lblMensagem.style.color = "#F2CB05";
+    lblMensagem.style.transition = "0.3s";
+    txtMensagem.style.borderColor = "#F2CB05";
+    txtMensagem.style.transition = "0.2s";
   }
